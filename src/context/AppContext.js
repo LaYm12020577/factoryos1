757bb385e58e4429049bf6e253a0edc0feeb53a7
LaYm12020577 <96872628+LaYm12020577@users.noreply.github.com>
2026-06-05 23:36:24 +0500

    Delete src directory

--- src/context/AppContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const LangCtx = createContext("uz");
-export const ThemeCtx = createContext({});
-
-export const useLang = () => useContext(LangCtx);
-export const useTheme = () => useContext(ThemeCtx);
--- src/hooks/useT.js
+++
@@ -1,7 +0,0 @@
-import { TRANSLATIONS } from '../constants/translations';
-import { useLang } from '../context/AppContext';
-
-export const useT = () => {
-  const lang = useLang();
-  return TRANSLATIONS[lang] || TRANSLATIONS.uz;
-};
