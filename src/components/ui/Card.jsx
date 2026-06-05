eb0772db9b2c040cc6842f67b6f8a2553f994c2d
LaYm12020577 <romancompotasty@gmail.com>
2026-06-05 23:34:46 +0500

    Удаление всех файлов и папок

--- src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Card = ({ children, className = "", noPadding = false }) => {
-  return (
-    <div className={`liquid-glass rounded-[2rem] overflow-hidden ${noPadding ? "" : "p-6"} ${className}`}>
-      {children}
-    </div>
-  );
-};
